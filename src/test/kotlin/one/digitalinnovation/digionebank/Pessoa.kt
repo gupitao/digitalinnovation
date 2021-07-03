43573a2d73b0e500087645a2436687a66d2410a8
Gustavo <gustavo.pitao@gumga.com.br>
2021-07-03 01:19:45 -0300

    update to talk about class members

--- src/test/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/test/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,21 +3,15 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome:String = "Gustavo"
     var cpf:String = "123.123.123-11"
-
     private set
 
-    inner class Endereco{
-        var rua:String = "Mitsuzo Tagushi"
-        var bairro:String = "Vila Nova"
-    }
+    constructor()
 
+    fun pessoaInfo() = "Nome: $nome, CPF: $cpf"
 }
 
 fun main(){
     val pGustavo = Pessoa();
 
-    println(pGustavo.nome);
-    println(pGustavo.cpf);
-    println(pGustavo.Endereco().rua);
-    println(pGustavo.Endereco().bairro);
+    println(pGustavo.pessoaInfo());
 }
