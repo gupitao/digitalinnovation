2f0b5b0ee9b0213b963237f1dde82ffdf6bf12e7
Gustavo <gustavo.pitao@gumga.com.br>
2021-07-03 01:12:25 -0300

    update apply the private modifier to set function cpf

--- src/test/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/test/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,6 +3,14 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome:String = "Gustavo"
     var cpf:String = "123.123.123-11"
+
+    private set
+
+    inner class Endereco{
+        var rua:String = "Mitsuzo Tagushi"
+        var bairro:String = "Vila Nova"
+    }
+
 }
 
 fun main(){
@@ -10,4 +18,6 @@ fun main(){
 
     println(pGustavo.nome);
     println(pGustavo.cpf);
+    println(pGustavo.Endereco().rua);
+    println(pGustavo.Endereco().bairro);
 }
