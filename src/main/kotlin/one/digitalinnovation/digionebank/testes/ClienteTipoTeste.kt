a7fddd4cc49b737e80be21d20c17e76c63fcb7f7
Gustavo <gustavo.pitao@gumga.com.br>
2021-07-03 22:51:31 -0300

    created teste document ClienteTipoTeste

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/ClienteTipoTeste.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/ClienteTipoTeste.kt
@@ -4,9 +4,14 @@ import one.digitalinnovation.digionebank.ClienteTipo
 
 fun main(){
     ClienteTipo.values().forEach {
-        println("$it.name $it") }
+        println("$it.name ${it.descricao}") }
 
     ClienteTipo.values().forEach { elemento ->
-        println("$elemento.name $elemento") }
+        println("$elemento.name ${elemento.descricao}") }
 
+    val pf = ClienteTipo.PF
+    println(">> ${pf.name} - ${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println(">> ${pj.name} - ${pj.descricao}")
 }
