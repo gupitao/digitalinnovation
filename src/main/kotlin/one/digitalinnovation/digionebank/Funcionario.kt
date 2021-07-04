15858168924a816f7963d051a9b9704fecee91df
Gustavo <gustavo.pitao@gumga.com.br>
2021-07-04 02:16:29 -0300

    Created sub classes manager and analyst

--- src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
@@ -5,9 +5,16 @@ import java.math.BigDecimal
 abstract class Funcionario(
     override val nome:String,
     override val cpf:String,
-    val salario:BigDecimal
+    val salario:Double
 ) : Pessoa(nome, cpf) {
 
-    abstract fun calculoAuxilio()
+    protected abstract fun calculoAuxilio(): Double
+
+    override fun toString(): String = """
+        Nome: $nome
+        CPF: $cpf
+        Salario: $salario
+        Auxilio: ${calculoAuxilio()}
+    """.trimIndent()
 
 }
